97f6c34f7deb5b9040aa603bda8367d55f84e113
Snorbo <140411654+Snorbo@users.noreply.github.com>
2025-04-06 08:55:41 +0800

    Update basic1.sh

--- basic1.sh
+++ basic1.sh
@@ -13,22 +13,4 @@ sudo tee /etc/apt/sources.list.d/nexttrace.list
 sudo apt-get update
 sudo apt-get install -y nexttrace
 
-echo -e "\n配置I/O调度器（仅适用于JustHost的vda磁盘）..."
-sudo tee /etc/systemd/system/set-io-scheduler.service >/dev/null <<'EOF'
-[Unit]
-Description=Set Disk Scheduler to None
-After=sysinit.target
-
-[Service]
-Type=oneshot
-ExecStart=/bin/sh -c 'echo "none" > /sys/block/vda/queue/scheduler'
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo chmod 644 /etc/systemd/system/set-io-scheduler.service
-sudo systemctl daemon-reload
-sudo systemctl enable --now set-io-scheduler
-
 echo -e "\n所有操作已完成！"
