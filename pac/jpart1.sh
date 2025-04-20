c28ea9f82a8d189aedc8e1d48066c84bf5f1a5b4
Snorbo <140411654+Snorbo@users.noreply.github.com>
2025-04-20 10:50:08 +0800

    Update jpart1.sh

--- pac/jpart1.sh
+++ pac/jpart1.sh
@@ -3,13 +3,13 @@ set -e
 wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/basic1.sh
 wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/basic2.sh
 wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/bbr.sh
-wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/nginx_t1.sh
-wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/acme_t1.sh
-wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/nginx_t2.sh
-wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/acme_t2.sh
-wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/nginx.conf
+wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/pac/nginx_t1.sh
+wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/pac/acme_t1.sh
+wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/pac/nginx_t2.sh
+wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/pac/acme_t2.sh
+wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/pac/nginx.conf
 wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/nginx_change.sh
-wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/hysteria2.sh
+wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/pac/hysteria2.sh
 wget https://raw.githubusercontent.com/Snorbo/public/refs/heads/main/adguard.sh
 chmod +x bbr.sh
 chmod +x basic1.sh
