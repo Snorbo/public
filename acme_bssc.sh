4c89e0d21896f3f73d084560e635d089c88baea8
Snorbo <140411654+Snorbo@users.noreply.github.com>
2025-11-20 17:13:41 +0800

    Update acme_bssc.sh

--- acme_bssc.sh
+++ acme_bssc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-DOMAIN="cloud.source.ffe.quest"
+DOMAIN="cloud.source.maya.locker"
 EMAIL="email.snorbo@gmail.com"
 NGINX_CONF="/usr/local/nginx/conf/nginx.conf"
 CERT_DIR="/usr/local/nginx/certs/${DOMAIN}"
